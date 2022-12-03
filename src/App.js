3c51c4e5c194248d9e36dd5f84993e835332f155
PrabhVirdi27 <virdiprabhsingh@gmail.com>
2022-12-03 00:59:22 -0500

    deployed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="App">
+
+      <h1 className="headinglarge">Problem 3 lab10 Prabh Virdi</h1>
+      <br></br>
+      <h3>How I installed the framework and built the page</h3>
+      <div className="steps">
+      <p>Step 1: Downloaded windows 64bit node.js version from wwww.nodejs.org.</p>
+      <p>Step 2: Installed the nodejs from the setup.</p>
+      <p>Step 3: Created a folder called "Node js apps" on my C drive.</p>
+      <p>Step 4: Used command line and went to the "Node js apps" folder.</p>
+      <p>Step 5: Used the command "npx create-react-app lab10c" to install react in the folder.</p>
+      <p>Step 6: Opened Visual Studio Code and opened the lab10c folder which was created.</p>
+      <p>Step 7: Opened the terminal in Visual Studio Code.</p>
+      <p>Step 8: Executed the command "npm start" and then started editing my lab10c website.</p>
+      </div>
+      <br></br>
+      <h3>Difficulties encountered and how I solved them</h3>
+      <div className="steps">
+      <p>Firstly, I had difficulties with the command line because I was having trouble going into 
+        the right folders needed to run the different commands in. I solved this by checking and double 
+        checking every folder which I was entering through the command line. 
+        <br></br>
+        Also, I had trouble with the commands themselves as they needed to be edited a bit to suit 
+        the app names I was creating along with 
+        how I wanted my app to work and start in react. I solved this by looking up how the command works before 
+        running it in the command line.
+      </p>
+      </div>
+      </body>
   );
 }
 
