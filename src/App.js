908621d2f1fd8ca10d04e99e9b5cbe4e0017ddf0
PrabhVirdi27 <virdiprabhsingh@gmail.com>
2022-12-03 16:25:07 -0500

    deployed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
       <h1 className="headinglarge">Problem 3 lab10 Prabh Virdi</h1>
       <br></br>
-      <h3>How I installed the framework and built the page</h3>
+      <h3>How I installed the framework(React)</h3>
       <div className="steps">
       <p>Step 1: Downloaded windows 64bit node.js version from wwww.nodejs.org.</p>
       <p>Step 2: Installed the nodejs from the setup.</p>
@@ -14,6 +14,20 @@ function App() {
       <p>Step 4: Used command line and went to the "Node js apps" folder.</p>
       <p>Step 5: Used the command "npx create-react-app lab10c" to install react in the folder.</p>
       <p>Step 6: Opened Visual Studio Code and opened the lab10c folder which was created.</p>
+      <p>Step 7: Opened the terminal in Visual Studio Code.</p>
+      <p>Step 8: Executed the command "npm start" and then started editing my lab10c page using the js and css files 
+        in the folder.</p>
+      </div>
+      <br></br>
+      <h3>How I built the page and hosted using github</h3>
+      <div className="steps">
+      <p>Step 1: made a repository in git called prabhvirdi530</p>
+      <p>Step 2: got git bash and typed gh pages</p>
+      <p>Step 3: then initialzed git</p>
+      <p>Step 4: made the remote origin to my git repository</p>
+      <p>Step 5: used npm run deploy to deploy project to github pages</p>
+      <p>Step 6: uploaded the actual code in a new branch called masted in the same repository</p>
+      <p>Step 7: Opened Visual Studio Code and opened the lab10c folder which was created.</p>
       <p>Step 7: Opened the terminal in Visual Studio Code.</p>
       <p>Step 8: Executed the command "npm start" and then started editing my lab10c website.</p>
       </div>
